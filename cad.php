b0c5f0f2f43e0256f6fd93fb14b0cdd6742f2c05
Jonathan <jonathan-516@hotmail.com>
2024-01-16 06:26:13 -0300

    update

--- cad.php
+++ cad.php
@@ -31,7 +31,7 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
     try {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.live.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
