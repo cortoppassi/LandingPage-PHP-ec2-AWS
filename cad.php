6d9ed7cc3de77deeb69e8981d6dc514cc7bb2778
Jonathan <jonathan-516@hotmail.com>
2024-01-16 07:23:32 -0300

    update

--- cad.php
+++ cad.php
@@ -17,48 +17,46 @@
 ?>
 
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
 
-    try {
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'john.chmod777@gmail.com';                     //SMTP username
-        $mail->Password   = 'Linux2024!';                               //SMTP password dpzp quvi luea mwkn
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;            //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'john.chmod777@gmail.com';                     //SMTP username
+    $mail->Password   = 'Linux2024!';                               //SMTP password dpzp quvi luea mwkn
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;            //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        // Ajuste no tempo limite
-        $mail->Timeout = 60;
-        // Um servidor SMTP (Simple Mail Transfer Protocol) é um servidor de correio eletrônico responsável por enviar e rotear e-mails na Internet.
-        
-        $mail->setFrom('john.chmod777@gmail.com', 'Jonathan');
-        $nome = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-        $mail->addAddress('jonathan-516@hotmail.com', '$nome');     //Destinatário
+    // Ajuste no tempo limite
+    $mail->Timeout = 60;
+    // Um servidor SMTP (Simple Mail Transfer Protocol) é um servidor de correio eletrônico responsável por enviar e rotear e-mails na Internet.
+    
+    $mail->setFrom('john.chmod777@gmail.com', 'Jonathan');
+    $nome = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $mail->addAddress('jonathan-516@hotmail.com', '$nome');     //Destinatário
 
-        $mail->addReplyTo('john.chmod777@gmail.com', 'Information');
+    $mail->addReplyTo('john.chmod777@gmail.com', 'Information');
 
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'IMERSÃO PHP SEM MISTÉRIOS';
-        $mail->Body    = 'This is the HTML message body <b>Uma jornada intensiva e imersiva no universo da programação web. Durante esse período concentrado, os participantes têm a oportunidade de aprimorar suas habilidades no desenvolvimento de aplicações dinâmicas usando PHP, uma linguagem versátil e amplamente utilizada.</b>';
-        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'IMERSÃO PHP SEM MISTÉRIOS';
+    $mail->Body    = 'This is the HTML message body <b>Uma jornada intensiva e imersiva no universo da programação web. Durante esse período concentrado, os participantes têm a oportunidade de aprimorar suas habilidades no desenvolvimento de aplicações dinâmicas usando PHP, uma linguagem versátil e amplamente utilizada.</b>';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-        echo 'E-mail enviado com sucesso!';
-    } catch (Exception $e) {
-        echo "Erro ao enviar o e-mail: {$e->getMessage()}";
-}
+    if (!$mail -> send()){
+        die ("Erro no envio do e-mail: {$mail -> ErrorInfo}");
+        }echo 'Mensagem enviada com sucesso';
+?>
